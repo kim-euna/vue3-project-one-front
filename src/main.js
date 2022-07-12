4ea0102b9658c4ea1f1637b98a8fbe5f87ef091d
kimeuna <eakim@osstem.com>
2022-07-12 11:05:32 +0900

    프로젝트 생성 및 부트스트랩 프레임워크 적용

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BootStrapVue from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
 createApp(App).mount('#app')
+createApp(App).use(BootStrapVue);
